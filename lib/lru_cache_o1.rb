731b01f248aafd2e47a8734fbd64041420b16752
blainelawson <blaine.lawson@gmail.com>
2019-11-15 14:32:14 -0500

    Began to refactor lru_cache class for O(1) time complexity

---
+++ lib/lru_cache_o1.rb
@@ -0,0 +1,58 @@
+require 'pry'
+
+class LruCacheO1
+    attr_accessor :put, :entries, :lru
+    attr_reader :get, :cap
+
+    @@all = []
+
+    def initialize(cap = 2)
+        @cap = cap
+        @entries = {}
+        @lru = []
+
+        @@all << self
+    end
+
+    def put(k, v)
+        if !self.entries.has_key?(k)
+            # binding.pry
+            self.entries.delete(self.lru.pop) if (self.entries.length + 1) > self.cap
+            
+            self.entries[k] = v
+
+            update_lru(k)
+        end
+    end
+
+    def get(k)
+        if self.entries.has_key?(k)
+            update_lru(k)
+            return self.entries[k]
+        else
+            return -1
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    private
+
+    def update_lru(k)
+        self.lru.unshift(self.lru.delete(k) || k)
+    end
+end
+
+cache = LruCacheO1.new(2)
+cache.put(1,1)
+cache.put(2,2)
+cache.get(1)
+cache.put(3,3)
+cache.get(2)
+cache.put(4,4)
+cache.get(1)
+cache.get(3)
+# binding.pry
+cache.get(4)
